f2447701e84656c794506f436dc43ef160e8b287
Sunil Kumar <sunilkumar55066@gmail.com>
2022-10-05 12:42:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var audio = new Audio('click.mp3');
+
+function click_m() {
+    audio.play();
+}
+function cal() {
+    let x = document.getElementById("display-panel").innerHTML;
+    let result = 0;
+    result = eval(x);
+    document.getElementById("display-panel").innerHTML = result;
+}
+
+function display(val) {
+    document.getElementById("display-panel").innerHTML += val;
+}
+
+function clear_m() {
+    document.getElementById("display-panel").innerHTML = "";
+}
